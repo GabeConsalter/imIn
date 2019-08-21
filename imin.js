4153a0c6eee69c59838e16c2000338e7cc6823eb
Gabriel Consalter <consaltergabriel@gmail.com>
2019-08-21 11:30:21 -0300

    Shell command convertion

--- imin.js
+++ imin.js
@@ -1,7 +1,9 @@
+#!/usr/bin/env node
+
 const program = require('commander');
 
 program
-	.command('hello')
+	.command('version')
 	.description('Hello to users')
 	.action(() => {
 		console.log('Hello Users!')
